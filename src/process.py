bb6ac33ebfb53a86db8ca7c5f6fa31e69afe049a
Zavins <zywang1@uci.edu>
2023-11-04 22:09:28 -0700

    Update Server

--- src/process.py
+++ src/process.py
@@ -1,18 +1,29 @@
-import openai
+#HackSC
+#Authors: Boshe Zhang, Yiqi Xue, Zhiyuan Zhangm, Ying Sun
 
-with open("./keys/openai.key") as f:
-    openai.api_key = f.readline()
+import openai
+openai.api_key = ('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')
 
 def process_data(text_data):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=[
-            {"role": "system", "content": "You are a helpful summerizer."},
-            {"role": "user", "content": text_data},
+            {"role": "system", "content": "You are a helpful mindmap maker, you can summerize the text into a mindmap."},
+            {"role": "user", "content": "Convert the following text into a mindmap, using markdown language, keep all key points"},
+            {"role": "user", "content": text_data}
         ]
     )
 
-    print(response.choices[0].message)
+    markdown_text = response.choices[0].message
 
-    return response.choices[0].message
+    return markdown_text
 
+
+# def prompt():
+#     openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful summerizer. "},
+#             {"role": "user", "content": "Summarize the following in markdown format."},
+#         ]
+#     )
--- src/server.py
+++ src/server.py
@@ -1,14 +1,26 @@
+#HackSC
+#Authors: Boshe Zhang, Yiqi Xue, Zhiyuan Zhangm, Ying Sun
+#
+
+import time
+
 from flask import Flask, request
+from flask_limiter import Limiter
 from process import process_data
 
 # 创建Flask应用
 app = Flask(__name__)
 
+limiter = Limiter(
+    app,
+    default_limits=["5 per minute"],  # 每分钟最多允许 5 次请求
+)
+
+
 # 定义路由和视图函数
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
 
 # @app.route('/api/data', methods=['GET'])
 # def get_data():
@@ -19,12 +31,15 @@ def hello_world():
 #     return data
 
 
-
 @app.route('/api/data', methods=['GET'])
 def post_data():
     # 从请求中获取数据
     #data = request.get_json()
-    text_data = request.data.decode('utf-8')
+    
+    #text_data = request.data.decode('utf-8')
+    
+
+    text_data = 'The dominant sequence transduction models are based on complex recurrent or convolutional neural networks in an encoder-decoder configuration. The best performing models also connect the encoder and decoder through an attention mechanism. We propose a new simple network architecture, the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely. Experiments on two machine translation tasks show these models to be superior in quality while being more parallelizable and requiring significantly less time to train. Our model achieves 28.4 BLEU on the WMT 2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU. On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models from the literature. We show that the Transformer generalizes well to other tasks by applying it successfully to English constituency parsing both with large and limited training data.'
 
     # # 检查是否有名为 'data' 的键
     # if 'data' in data:
@@ -33,6 +48,7 @@ def post_data():
     # else:
     #     return jsonify({'error': 'No "data" key in the request'})
 
+    
     processed_data = process_data(text_data)
 
     return processed_data
@@ -41,3 +57,5 @@ def post_data():
 # 运行应用
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
